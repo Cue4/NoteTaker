1867835b73fb79d6291a7f84af2a10b54c477fd0
Caliph Ferguson <caliphferguson@gmail.com>
2024-08-12 14:47:13 -0400

    updated server file

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const data = require('./Develop/db/db.json');
+const data = require('./db/db.json');
+const {v4:uuidv4} = require("uuid")
 const PORT = 3001;
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
 
  // Read the contents of the db.json file
 app.get('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname, './Develop/db/db.json'), 'utf8', (err, data) => {
+    fs.readFile(path.join(__dirname, './db/db.json'), 'utf8', (err, data) => {
         if (err) {
             console.error(err);
             return res.status(500).json({ error: 'Internal Server Error' });
@@ -19,44 +22,40 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    newNote.id = uuidv4();
-    fs.readFile(path.join(__dirname, './Develop/db/db.json'), 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Internal Server Error' });
-        }
-        const notes = JSON.parse(data);
+ 
+        const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8' ))
+        const newNote = {
+            title:req.body.title, 
+            text: req.body.text,
+            id: uuidv4()
+        };
         notes.push(newNote);
-        fs.writeFile(path.join(__dirname, 'db.json'), JSON.stringify(notes, null, 2), (err) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).json({ error: 'Internal Server Error' });
-            }
-            res.json(newNote);
-        });
-    });
+       fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+        res.json(notes);
+         
 });
 
-app.get('notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+app.delete('/api/notes/:id', async(req, res) => {
+    try {
+        const notesData = await JSON.parse (fs.readFileSync("./db/db.json", "utf8"))
+        const filteredNotes = notesData.filter((note) => {
+            return note.id !== req.params.id
+        })
+        fs.writeFileSync("./db/db.json", JSON.stringify(filteredNotes))
+        res.status(200).json({message:"Note Deleted"})
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('db', (req, res) => res.json(data));
-
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
   });
-
-
-app.delete('/api/notes:id', async(req, res) => {
-    const { id } = req.params;
-    const result = await db.query('DELETE FROM notes WHERE id = $1 RETURNING *', [id]);
-    if (result.rows.length === 0) {
-        return res.status(404).json({ error: 'Tag not found' });
-    }
-    res.status(204).send();  
-});
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
